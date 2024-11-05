fa6edcf80de0acbb26dcd65a17c375bcdf3e103c
Aishwarya Rashinkar <aishwaryarashinkar487@gmail.com>
2024-11-05 11:12:58 +0000

    BASIC UI

--- project1/webapp/controller/View1.controller.js
+++ project1/webapp/controller/View1.controller.js
@@ -7,6 +7,44 @@ function (Controller) {
     return Controller.extend("project1.controller.View1", {
         onInit: function () {
 
+        },
+        onPress: function() {
+            // alert("Hey");
+           this.getOwnerComponent().getRouter().navTo("RouteView2");
+           location.reload();
+        },
+        onSubmit: function () {
+            let name = this.getView().byId("inp1").getValue();
+            let msg = "welcome " + name;
+            let alignment = this.getView().byId("inp2").getValue();
+            this.getView().byId("_IDGenText").setText(msg);
+            this.getView().byId("_IDGenText").setTextAlign(alignment);
+            this.getView().byId("inp1").setEnabled(false);
+            this.getView().byId("inp2").setEnabled(false);       
+        },
+        onHideShow: function () {
+
+            let text = "Hide";
+            let text1 = this.getView().byId("_IDGenButton3").getText(text);
+
+            if(text1 === "Hide"){
+                this.getView().byId("_IDGenText").setVisible(false);
+                this.getView().byId("lbl1").setVisible(false);
+                this.getView().byId("lbl2").setVisible(false);
+                this.getView().byId("inp1").setVisible(false);
+                this.getView().byId("inp2").setVisible(false);
+    
+                this.getView().byId("_IDGenButton3").setText("Show");
+            }
+            else {
+            this.getView().byId("_IDGenText").setVisible(true);
+            this.getView().byId("lbl1").setVisible(true);
+            this.getView().byId("lbl2").setVisible(true);
+            this.getView().byId("inp1").setVisible(true);
+            this.getView().byId("inp2").setVisible(true);
+
+            this.getView().byId("_IDGenButton3").setText("Hide");
+            }
         }
     });
 });
